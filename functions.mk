e8d1a8f377ac7730e75acbc914e7f91c43d27146
Patrick Selge <patrick.selge@th-wildau.de>
2016-12-19 09:14:14 +0100

    add the echoed command call

--- functions.mk
+++ functions.mk
@@ -18,8 +18,8 @@ uc = $(shell echo $(1) | tr a-z A-Z)
 #
 # @params: Object Class: e.g. pv, pvc, etc..
 #################################################################################
-oc_params = $(foreach param, \
-            $(filter $1_%,$(.VARIABLES)), \
+oc_params = $(foreach param,\
+            $(filter $1_%,$(.VARIABLES)),\
             -v $(subst $1_,,$(param))=$($(param)))
 
 #################################################################################
@@ -36,8 +36,7 @@ oc_params = $(foreach param, \
 yml_oc_importer = $(foreach V,\
                   $(call yml_template_files,$1),\
                   @echo "--> Creating $(call uc,$1)s from $(V):"; \
-									echo "----> Used Parameters: $(call oc_params,$1)"; \
-                  oc process -f $V $(call oc_params,$1);)
+									$(call echoed_call,oc process -f $V $(call oc_params,$1));)
 
 #################################################################################
 # calls the parent image if its folder exists
@@ -47,3 +46,10 @@ yml_oc_importer = $(foreach V,\
 # 			     separate it's environment
 #################################################################################
 call_parent = $(if $(realpath $1), @(/bin/bash -c "make -C $1 $2"))
+
+#################################################################################
+# Prints out the command befire executing it
+#
+# @params: command to be executed and echoed
+#################################################################################
+echoed_call = (echo "----> $1"; $1)
